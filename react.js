200f8aacc55d2cf6733f2392e8c24726e7ad6c49
Omri Uday <omriudayDev@gmail.com>
2022-06-12 12:30:15 +0300

    testt2

--- react.js
+++ react.js
@@ -1,5 +1,9 @@
-const test1 = "test";
+const test2 = "test";
 // git commit -m "first commit"
 // git branch -M main
 // git remote add origin https://github.com/OmriUday/react.git
 // git push -u origin main
+
+// git commit -m "test2"
+// git commit --amend --date="2022-06-12 12:30:15"
+// git push -f origin main
